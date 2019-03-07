8074b944dfad06b0cfb7ba396396e80342584899
mohamedHedi1990 <med.elhadi.boussada@supcom.tn>
2019-03-07 13:24:53 +0100

    second commit

--- src/main/java/org/test/test/App.java
+++ src/main/java/org/test/test/App.java
@@ -18,26 +18,27 @@ public class App
 {
     public static void main( String[] args ) throws AddressException, MessagingException
     {
+    	//////
     	Properties prop = new Properties();
     	prop.put("mail.smtp.auth", "true");
     	prop.put("mail.smtp.starttls.enable", "true");
     	prop.put("mail.smtp.host", "smtp.gmail.com");
     	prop.put("mail.smtp.port", "587");
-    	prop.put("mail.smtp.user", "mohamedelhadiboussada@gmail.com");
-        prop.put("mail.smtp.password", "rOu7daydo2018");
+    	prop.put("mail.smtp.user", "");
+        prop.put("mail.smtp.password", "");
         
         Session session = Session.getDefaultInstance(prop);
         MimeMessage message = new MimeMessage(session);
         
-        message.setFrom(new InternetAddress("mohamedelhadiboussada@gmail.com"));
+        message.setFrom(new InternetAddress(""));
         
-        InternetAddress toAddress = new InternetAddress("biservices010@gmail.com");
+        InternetAddress toAddress = new InternetAddress("");
         message.addRecipient(Message.RecipientType.TO, toAddress);
         
         message.setSubject("mail test");
         message.setText("mail test body");
         Transport transport = session.getTransport("smtp");
-        transport.connect("smtp.gmail.com", "mohamedelhadiboussada@gmail.com", "rOu7daydo2018");
+        transport.connect("smtp.gmail.com", "from", "pass");
         transport.sendMessage(message, message.getAllRecipients());
         transport.close();
         System.out.println( "Hello World!" );
